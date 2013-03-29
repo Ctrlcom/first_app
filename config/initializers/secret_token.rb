8fc42e69e7ad23d1342e25c1d7d8796a9b0aeddd
Ctrlcom <contact@ctrl-communication.com>
2013-03-29 09:32:03 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '19fd7d9e978f8a86be3621cd609d1addffb193715ae3454a835d5607774ec8278b7c89f747d4d8c6aeb65ee56e7374282db3a4aa69ea17b729f6b5e37604efbf'
